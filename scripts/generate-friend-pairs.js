6a4484ae54ded6fe4630c002aac3888381f055e2
NSorokovaya <nadiaasorokovaya@gmail.com>
2024-06-06 17:58:48 +0200

    added generate-users-email-script

---
+++ scripts/generate-friend-pairs.js
@@ -0,0 +1,9 @@
+(async () => {
+  // 1. select all users
+  // 2. generate random friends pairs betweens users
+  // 2.1. each user should have min 5 friends
+  // 2.2. user can't friends with himself
+  // 2.3. friend pairs should be unique
+
+  console.log("hello");
+})();
